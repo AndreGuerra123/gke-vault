7ee05f6c84aff3e399d491fecaf010c539d7d801
André Catarino Guerra <guerraandre@hotmail.com>
2019-05-30 18:11:41 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -62,3 +62,4 @@ spec:
 EOF
 kubectl apply -f vault-load-balancer.yaml
 export VAULT_TOKEN=$(gsutil cat gs://${GCS_BUCKET_NAME}/root-token.enc | base64 --decode | gcloud kms decrypt --project ${PROJECT_ID} --location global --keyring vault --key vault-init --ciphertext-file - --plaintext-file - )
+
